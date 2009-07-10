583278570fd9bf4c858029f284648eeae0647de6
Brandon Niemczyk <brandon@brandon-laptop.(none)>
2009-07-10 02:34:07 -0400

    added some SK optimizations

--- SPJ/LC/Runtime.hs
+++ SPJ/LC/Runtime.hs
@@ -9,3 +9,9 @@ _I x = x
 _K c x = c
 
 _S f g x = f x (g x)
+
+_B f g x = f (g x)
+
+_C f g x = f x g
+
+_S' c f g x = c (f x) (g x)
--- SPJ/LC/SK.hs
+++ SPJ/LC/SK.hs
@@ -1,10 +1,10 @@
-module SPJ.LC.SK where
+module SPJ.LC.SK (removeAbstractions) where
 
 import SPJ.LC.Abslc
 import SPJ.LC.WalkExp
 
 removeAbstractions :: Exp -> Exp
-removeAbstractions exp = (walkExp removeAbstractions') exp
+removeAbstractions exp = optimize $ (walkExp removeAbstractions') exp
     where
         removeAbstractions' (AbstractionTerm id body) = case body of
             ApplicationTerm e1 e2 -> s id e1 e2
@@ -23,3 +23,34 @@ removeAbstractions exp = (walkExp removeAbstractions') exp
         i = VariableTerm $ Identifier "_I"
 
         k c = ApplicationTerm (VariableTerm $ Identifier "_K") c
+
+optimize :: Exp -> Exp
+optimize = iterative $ walkExp $ skk_opt . b_opt . c_opt . s_opt
+
+skk_opt (ApplicationTerm
+    (ApplicationTerm (VariableTerm (Identifier "_S")) (ApplicationTerm (VariableTerm (Identifier "_K")) p))
+    (ApplicationTerm (VariableTerm (Identifier "_K")) q)) =
+        ApplicationTerm (VariableTerm (Identifier "_K")) (ApplicationTerm p q)
+skk_opt e = e
+
+b_opt (ApplicationTerm
+    (ApplicationTerm (VariableTerm (Identifier "_S")) (ApplicationTerm (VariableTerm (Identifier "_K")) p))
+    q) =
+        ApplicationTerm (ApplicationTerm (VariableTerm (Identifier "_B")) p) q
+b_opt (ApplicationTerm (ApplicationTerm (VariableTerm (Identifier "_B")) p) (VariableTerm (Identifier "_I"))) = p
+b_opt e = e
+
+c_opt (ApplicationTerm
+    (ApplicationTerm (VariableTerm (Identifier "_S")) p)
+    (ApplicationTerm (VariableTerm (Identifier "_K")) q)) = 
+        ApplicationTerm (ApplicationTerm (VariableTerm (Identifier "_C")) p) q
+c_opt e = e
+
+s_opt (ApplicationTerm
+    (ApplicationTerm 
+        (VariableTerm (Identifier "_S")) 
+        (ApplicationTerm (ApplicationTerm (VariableTerm (Identifier "_B")) x) y))
+        z) = (ApplicationTerm
+                (ApplicationTerm (ApplicationTerm (VariableTerm (Identifier "_S'")) x) y)
+                z)
+s_opt e = e
